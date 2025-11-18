7f84ea1fd36b0032194cf5febb67f0c8563da989
Maria Emanuele Carvalho de Araújo <mariaemanuelecarvalhodearaujo@gmail.com>
2025-11-18 20:08:44 -0300

    Organização de pastas

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import os
-
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return send_from_directory('src/templates', 'index.html')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=81, debug=True)
