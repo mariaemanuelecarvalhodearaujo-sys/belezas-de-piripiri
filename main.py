20048c61e2bb1d7bfa55c239cb99f6d45e9f7b01
Maria Emanuele Carvalho de Araújo <mariaemanuelecarvalhodearaujo@gmail.com>
2025-11-25 08:50:42 -0300

    Primeiras estilizações

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ app = Flask(__name__,static_folder='src/static', template_folder='src/templates'
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_file('src/static/icons/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81, debug=True)
